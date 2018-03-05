76d85ba75c53ea93e153a900eafce07a3be52f0e
maciekkobierecki <maciekkobierecki@gmail.com>
2018-03-05 17:52:12 +0100

    0.0.5

--- found-your-dog/src/components/Menu.js
+++ found-your-dog/src/components/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Color} from './Color';
 import { MenuButton } from './MenuButton';
+import { Icon, ICONS, SIZE } from './Icons';
+
 
 const style=({
     display: 'flex',
@@ -12,7 +14,7 @@ const style=({
 export class Menu extends React.Component {
     render(){
         return <div style={style}>
-            <MenuButton label="szukam" />
+            <MenuButton label={<Icon icon={ICONS.CAMERA}  defaultSize={SIZE.CAMERA}/>}/>
             <MenuButton label="dodaje" />
         </div>;
     }
